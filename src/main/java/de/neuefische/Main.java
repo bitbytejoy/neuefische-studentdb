ada232fff2002b12e9b4fb157c2c760805957854
bitbytejoy <bitbytejoy@gmail.com>
2022-11-30 12:56:12 +0100

    fix: Tricky hidden trix

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,14 +1,14 @@
 package de.neuefische;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Student> students = Map.of(
-                "1", new Student("1", "Bob"),
-                "2", new Student("2", "Tom"),
-                "3", new Student("3", "Jane")
-        );
+        Map<String, Student> students = new HashMap<>();
+        students.put("1", new Student("1", "Bob"));
+        students.put("2", new Student("2", "Tom"));
+        students.put("3", new Student("3", "Jane"));
 
         StudentDB studentDB = new StudentDB(students);
 
